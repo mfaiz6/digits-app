2e3fb89c0a2b92b568073d28e7358ccd03b42a9e
mfaiz6 <Mohammadfaiz_2019bcse002@nitsri.net>
2023-03-29 22:17:41 +0530

    Revert "add responsive hero component"
    This reverts commit 6597c533f9f43340720b12b9f1b994268f0804a9.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
-import Partners from './components/partners/Partners';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Partners />
     </div>
   );
 }
